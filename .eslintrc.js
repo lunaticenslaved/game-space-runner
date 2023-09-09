cb2de38c13fbb62e7adddeda07c58eb31ce23ae1
lunaticenslaved <lunaticenslaved@yandex.ru>
2023-09-09 23:47:25 +0300

    chore(lint): fix linting files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,23 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 2018,
+    "sourceType": "module",
+    "project": "tsconfig.json",
+    "warnOnUnsupportedTypeScriptVersion": false
   },
-  plugins: ['@typescript-eslint', 'react-hooks'],
+  plugins: [
+    "prettier",
+    "@typescript-eslint", 
+    'react-hooks'
+  ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
     'react-hooks/rules-of-hooks': 'error', // For checking rules of hooks
@@ -22,5 +33,5 @@ module.exports = {
       }
     ]
   },
-  ignorePatterns: ['dist', 'node_modules'],
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.js'],
 };
